af1b97b4a4a9ec17961c7aa36659f12a11651837
hamzashahab1610 <hamzashahab1610@gmail.com>
2020-08-13 11:53:28 +0500

    Modified field names in backend code according to db schema

--- controllers/orgController.js
+++ controllers/orgController.js
@@ -15,16 +15,16 @@ exports.list_all_orgs = function (req, res) {
 
 exports.create_org = function (req, res) {
   var saltRounds = 10;
-  var username = req.body.username;
+  var name = req.body.name;
   var email = req.body.email;
-  var phone_no = req.body.phone_no;
+  var phone = req.body.phone;
   var password = req.body.password;
   var hashedPassword = bcrypt.hashSync(password, saltRounds);
 
   var newOrg = {
-    username: username,
+    name: name,
     email: email,
-    phone_no: phone_no,
+    phone: phone,
     password: hashedPassword,
   };
 
@@ -37,11 +37,11 @@ exports.create_org = function (req, res) {
 };
 
 exports.login = function (req, res) {
-  //   var username = req.body.username;
+  //   var name = req.body.name;
   var email = req.body.email;
   var password = req.body.password;
 
-  //   const user = { name: username };
+  //   const user = { name: name };
 
   sql.query("SELECT * FROM email WHERE email = ?", [email], function (
     error,
@@ -58,7 +58,7 @@ exports.login = function (req, res) {
       if (results.length > 0) {
         if (
           bcrypt.compareSync(password, results[0].password) &&
-          results[0].username.length >= 0
+          results[0].name.length >= 0
         ) {
           console.log("The solution is: ", results);
           res.status(200).send({
