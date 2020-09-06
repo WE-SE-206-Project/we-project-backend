da5d2d9febf17e07fb58b16648eb46eadaecfff8
hamzashahab1610 <hamzashahab1610@gmail.com>
2020-09-06 11:31:04 +0500

    .

--- db.js
+++ db.js
@@ -1,29 +1,24 @@
 var mysql = require("mysql");
 
 var connection = mysql.createPool({
-  host: "we-project-backend.mysql.database.azure.com",
-  user: "hamzashahab1610@we-project-backend",
-  password: "Admin123",
-  database: "we-project-backend",
-  port: 3306,
-  ssl: {
-    // DO NOT DO THIS
-    // set up your ca correctly to trust the connection
-    rejectUnauthorized: false,
-  },
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "we-project-backend",
+	port: 3306,
 });
 
 connection.getConnection(function (err, connection) {
-  if (err) {
-    connection.release();
-    console.log(" Error getting mysql_pool connection: " + err);
-    throw err;
-  } else {
-    console.log("DB connection successful");
-    connection.query("SELECT * FROM user;", function (err, rows, fields) {
-      console.log("USERS: ", JSON.stringify(rows));
-    });
-  }
+	if (err) {
+		connection.release();
+		console.log(" Error getting mysql_pool connection: " + err);
+		throw err;
+	} else {
+		console.log("DB connection successful");
+		connection.query("SELECT * FROM user;", function (err, rows, fields) {
+			console.log("USERS: ", JSON.stringify(rows));
+		});
+	}
 });
 
 module.exports = connection;
